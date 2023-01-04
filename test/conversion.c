2c5b97df04d762fa74c1fc2976725a43a89fdd36
David Haworth <dh@thelancashireman.org>
2023-01-04 17:47:24 +0100

    Add/adjust license header

--- test/conversion.c
+++ test/conversion.c
@@ -1,3 +1,22 @@
+/* conversion.c - test program for the DS18B20 conversion function
+ *
+ * (c) David Haworth
+ *
+ *  This file is part of one-wire.
+ *
+ *  one-wire is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  one-wire is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with one-wire.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <stdio.h>
 
 #define NCONV 10
